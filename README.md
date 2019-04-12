17ef585d31756ca1225ef268614b077e7e5a3dfc
akashrajkn <akashrajkn@gmail.com>
2019-04-12 17:56:50 +0200

    Defined Gifcard and Profile models

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 1. python v3.7.x
 2. django v2.1.7
 3. miniconda
+4. Pillow
 
 ```
 conda env create -f environment.yml
--- backend/mysite/giftcards/models.py
+++ backend/mysite/giftcards/models.py
@@ -1,12 +1,35 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Giftcard(models.Model):
+    '''
+    Model describes the Giftcard
+    '''
+    barcode   = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner     = models.ForeignKey('Profile', on_delete=models.CASCADE)
+
+    redeemed  = models.BooleanField('Is the card redeemed?')
+    created   = models.DateTimeField('Date of creation')
+    activated = models.DateTimeField('Activated on')
+    validity  = models.DateTimeField('Valid until')
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Profile(models.Model):
+    '''
+    Profile can be any of {'Super', 'PensionEntity', 'Participant'}
+
+    'Super'        : is the superuser. For development purposes
+    'PensionEntity': can be something like "APG"
+    'Participant'  : is the customer. They can link their Digid to the system, view
+                     projections, etc.
+    '''
+    user         = models.OneToOneField(User, on_delete=models.CASCADE)
+    email        = models.EmailField(max_length=100)
+    image        = models.ImageField()   # TODO
+    profile_type = models.CharField(max_length=30, default='Participant')
+    bio          = models.TextField(max_length=500, blank=True)
+    location     = models.CharField(max_length=30, blank=True)
+    birth_date   = models.DateField(null=True, blank=True)
