3e015a41e5de4466867a7d7a7223af20ccacecfe
akashrajkn <akashrajkn@gmail.com>
2019-04-12 22:08:15 +0200

    POST - authentication API initial for mockDigiD app

--- backend/mysite/mockdigid/views.py
+++ backend/mysite/mockdigid/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Participant
+
+
+@api_view(['POST'])
+def connect_digid(request):
+    '''
+    Check if login credentials are right
+    '''
+    return Response({'valid':'asdfasf'}, status=status.HTTP_200_OK)
