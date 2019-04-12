369e9c7df5c3ae789845034949088cca3404263a
Leon <leon.de.zwart@webmi.nl>
2019-04-12 14:12:42 +0200

    Scaffold Redeem view

---
+++ frontend-vue/src/views/Redeem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="redeem">
+    <input type="text" v-model="redeemCode" />
+    <input type="submit" value="Redeem code">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Redeem extends Vue {
+  private redeemCode: string = '';
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
