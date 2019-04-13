345273fb16e6821e9e67212da0b1c0cd41409f23
akashrajkn <akashrajkn@gmail.com>
2019-04-13 12:01:51 +0200

    Created PensionFund model

--- backend/boldapi/mockdigid/models.py
+++ backend/boldapi/mockdigid/models.py
@@ -1,4 +1,4 @@
-import uuid
+import json
 
 from django.contrib.auth.models import User
 from django.db import models
@@ -15,3 +15,26 @@ class Participant(models.Model):
 
     def __str__(self):
         return self.user.get_username()
+
+
+class PensionFund(models.Model):
+    '''
+    Details of the pension fund
+    '''
+    bsn        = models.CharField(max_length=100, primary_key=True)
+    fund_name  = models.CharField(max_length=100)
+    active     = models.BooleanField(default=False)
+
+    ascription = models.CharField(max_length=100, null=True)
+    eligible   = models.BooleanField(default=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date   = models.DateField(null=True, blank=True)
+
+    fulltime_salary = models.FloatField(default=0.0)
+    entitlements    = models.CharField(max_length=1000)
+
+    def set_entitlements(self, x):
+        self.entitlements = json.dumps(x)
+
+    def get_entitlements(self, x):
+        return json.loads(self.entitlements)
--- backend/boldapi/mockdigid/serializers.py
+++ backend/boldapi/mockdigid/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Participant
+from .models import Participant, PensionFund
 
 
 class ParticipantsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participant
         fields = ("user")
+
+class PensionFundsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PensionFund
+        fields = ("fund_name", "active", "start_date", "end_date")
